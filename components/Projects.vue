fd74ff8518a87e57e1d8c47a68a95d52eebf9f53
Hermann Witte <hermann.witte@alpha-lab.net>
2026-02-18 17:15:16 +0100

    wip zwisschenstand

---
+++ components/Projects.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="w-full h-[150vh]">
+    <h1 class="text-6xl font-bold pl-24 pb-4">
+      Get a look into my work  
+    </h1>
+    <div class="w-full flex">
+      <div class="w-full h-[600px] bg-red-400">
+        f
+      </div>
+      <div class="w-full h-[600px] bg-blue-400">
+        f
+      </div>
+      <div class="w-full h-[600px] bg-green-400">
+        f
+      </div>
+    </div>
+  </div>
+</template> 
